0522d829e813a26ae2ead48f469afc3ae5f7cec6
bolshakov863 <ivan.bolshakov113@gmail.com>
2025-03-01 15:11:40 +0300

    add illision

--- rects.py
+++ rects.py
@@ -1,21 +1,34 @@
-import pygame
 import random
 from all_colors import *
+import pygame
 pygame.init()
-screen_width = 1280
-screen_height = 720
-pygame.display.set_caption('Квадрат_иллюзия')
-screen = pygame.display.set_mode((screen_width, screen_height))
-screen.fill((255, 255, 255))
-COLORS = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (255, 0, 255), (0, 0, 0)]
-pygame.display.flip()
+WIDTH = 1280
+HEIGHT = 720
+size = (1280, 720)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Иллюзия")
+BACKGROUND = WHITE
+screen.fill(BACKGROUND)
+x = 0
+y = 0
+rect_size = 200
+colors = COLORS
+FPS = 60
+clock = pygame.time.Clock()
+timer = 0
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    x = screen_width
-    y = screen_height
-    pygame.draw.rect(screen, COLORS, (x, y, 100, 100))
-    pygame.draw.rect(screen, COLORS, (x + 25, y + 25, 50, 50))
+    size = 200
+    for _ in range(19):
+        pygame.draw.rect(screen, random.choice(COLORS), (WIDTH // 2 - size // 2, HEIGHT // 2 - size // 2, size, size),
+                         2)
+        size -= 10
+
     pygame.display.flip()
+    clock.tick(FPS)
+
+pygame.quit()
+
